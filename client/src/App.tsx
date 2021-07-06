17b368544ae8c38a09a9e66d380e6d20700d7101
Thendog26 <epicnathan26@gmail.com>
2021-07-06 10:48:08 +0930

    architecture

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import './App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Route/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
